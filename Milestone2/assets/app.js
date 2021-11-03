6f4737c8e7997a2201fdfad7ad55696efb5a96ea
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-03 17:18:00 +0100

    problema messaggi dinamici

--- Milestone2/assets/app.js
+++ Milestone2/assets/app.js
@@ -1,11 +1,11 @@
+/* *
+MILESTONE2
 
+- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione.
 
-
-
-
-
-
-
+- Click sul contatto mostra la conversazione del contatto cliccato.
+*/
 
 const app = new Vue({
     el: '#app',
